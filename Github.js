8482a7bac90aa3085e4006c11c27bb66fb089e3c
Tonin <96670188+TheTonin@users.noreply.github.com>
2022-03-15 21:59:23 -0300

    Update Github.js

--- Github.js
+++ Github.js
@@ -1,5 +1,5 @@
 {
- "version":"2.22.6.4",
+ "version":"2.22.5.72",
  "title":"Update available 🛠️",
  "description":"A new update is available and this update includes bug fixes and new features.",
  "download":"https://t.me/twhatsappmods"
